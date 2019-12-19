2936ddad87fbbbbf67a75049cd62ca6f5bb42781
Kyle Dinh <kyled7@users.noreply.github.com>
2019-12-20 00:06:58 +0700

    Update update.ts

--- update.ts
+++ update.ts
@@ -1,14 +1,10 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import * as response from "../../libs/response";
-import Model, { validate } from "./model";
+import Model from "./model";
 
 export const handle: APIGatewayProxyHandler = async (event) => {
   const id = event.pathParameters.id
   const data = JSON.parse(event.body)
-
-  if (!validate(data)) {
-    return response.error(400, "Wrong input data!")
-  }
 
   const result = await Model.update(id, data)
   if (result === null) {
