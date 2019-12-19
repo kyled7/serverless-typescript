ccf046058f7cfa6f1a3aa9852c45de86e8bafb43
Kyle Dinh <kyledinh.vn@gmail.com>
2019-12-19 23:44:10 +0700

    Update template with DynamoDB

--- create.ts
+++ create.ts
@@ -1,13 +1,9 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import * as response from "../../libs/response";
-import Model, { validate } from "./model";
+import Model from "./model";
 
 export const handle: APIGatewayProxyHandler = async (event) => {
   const data = JSON.parse(event.body)
-
-  if (!validate(data)) {
-    return response.error(400, "Wrong input data!")
-  }
 
   const result = await Model.create(data)
   if (result === null) {
--- model.ts
+++ model.ts
@@ -1,21 +1,5 @@
 require('dotenv').config();
 import Database from "../../libs/database";
 
-const DB = new Database(process.env.MONGODB_URL, process.env.MONGODB_DB, process.env.MONGODB_COLLECTION)
+const DB = new Database(process.env.DYNAMODB_TABLE)
 export default DB
-
-export interface YourObject {
-  title: string,
-  description?: string
-}
-
-export const validate = (data: YourObject) : boolean => {
-  //Write your validation here
-  if (!data.title) {
-    return false
-  }
-  if (data.title.length > 10) {
-    return false
-  }
-  return true
-}
